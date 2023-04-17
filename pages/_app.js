0fca12d3d25b7af19beeab6e078c835bf9022e75
andr-ew <andrewcshike@gmail.com>
2023-04-16 23:22:15 -0500

    setup, copy components from andr-ew/wrms_live

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,32 @@
-import '@/styles/globals.css'
+import { Global, css } from '@emotion/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Global
+                styles={css`
+                    * {
+                        box-sizing: border-box;
+                    }
+
+                    html,
+                    body {
+                        width: 100%;
+                        height: 100%;
+                        margin: 0;
+                        padding: 0;
+                    }
+
+                    body {
+                        background: #3e54d7;
+                    }
+
+                    video {
+                        object-fit: cover;
+                    }
+                `}
+            />
+            <Component {...pageProps} />
+        </>
+    );
 }
